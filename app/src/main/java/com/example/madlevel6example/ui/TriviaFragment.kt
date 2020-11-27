f0778492c14da8398801d310e0ce864360e4716f
Sven van Putten <svenvanputten@gmail.com>
2020-11-27 13:45:39 +0100

    Updated the TriviaFragment to observe and show changes made to the viewModel

--- app/src/main/java/com/example/madlevel6example/ui/TriviaFragment.kt
+++ app/src/main/java/com/example/madlevel6example/ui/TriviaFragment.kt
@@ -1,16 +1,23 @@
 package com.example.madlevel6example.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.madlevel6example.R
+import com.example.madlevel6example.viewModel.TriviaViewModel
+import kotlinx.android.synthetic.main.fragment_trivia.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class TriviaFragment : Fragment() {
+
+    private val viewModel: TriviaViewModel by activityViewModels()
+
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +29,19 @@ class TriviaFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        observeTrivia()
     }
+
+    private fun observeTrivia() {
+        viewModel.trivia.observe(viewLifecycleOwner, {
+            tvTrivia.text = it?.text
+        })
+
+        // Observe the error message.
+        viewModel.errorText.observe(viewLifecycleOwner, {
+            Toast.makeText(activity, it, Toast.LENGTH_SHORT).show()
+        })
+    }
+
 }
